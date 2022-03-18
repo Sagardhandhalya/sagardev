271c0c9e0b72fedf8c9ed8001f5657f0fc5af5c6
Sagardhandhalya <dhandhalyasagar0@gmail.com>
2022-03-18 19:00:46 +0530

    first commit

---
+++ styles/theme.tsx
@@ -0,0 +1,19 @@
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const config: ThemeConfig = {
+  ...colors,
+  initialColorMode: "dark",
+  useSystemColorMode: true,
+};
+
+const theme = extendTheme({ config });
+
+export default theme;
