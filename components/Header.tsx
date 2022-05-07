78cefa129200fac65306c2aa60ed988810c07ff9
Sagardhandhalya <dhandhalyasagar0@gmail.com>
2022-05-07 20:58:07 +0530

    alt props added

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,29 +11,39 @@ import {
   Icon,
   Box,
   MenuButton,
-  Divider
+  Divider,
 } from "@chakra-ui/react";
 import Image from "next/image";
-import fco from './../public/fco.png'
-import { useState } from 'react';
+import fco from "./../public/fco.png";
+import { useState } from "react";
 
 const Header = () => {
   const { colorMode, toggleColorMode } = useColorMode();
-  const [isOpen, setIsOpen] = useState(false)
-const color = useColorMode()
+  const [isOpen, setIsOpen] = useState(false);
+  const color = useColorMode();
   return (
-    <Box position="fixed" top="0px" zIndex="99" w="100%" style={{ backdropFilter: "blur(10px)" }}>
+    <Box
+      position="fixed"
+      top="0px"
+      zIndex="99"
+      w="100%"
+      style={{ backdropFilter: "blur(10px)" }}>
       <Flex align="center" justify="space-around" mt="4">
-        <Flex align="center" >
+        <Flex align="center">
           <Link href="/" mb="-2" mr="2">
-            <Image src={fco} width="20" height="30" />
+            <Image src={fco} width="20" height="30" alt="header logo" />
           </Link>
           <Text as="h2" fontWeight="semibold" fontSize="md">
             Sagar Dhandhalya
           </Text>
         </Flex>
         <div>
-          <ButtonGroup display={["none", "none", "inline", "inline"]} variant="outline" mr="4" spacing="3" colorScheme="blue">
+          <ButtonGroup
+            display={["none", "none", "inline", "inline"]}
+            variant="outline"
+            mr="4"
+            spacing="3"
+            colorScheme="blue">
             <Link href="/blog">
               <Button>Blogs</Button>
             </Link>
@@ -42,32 +52,52 @@ const color = useColorMode()
             </Link>
           </ButtonGroup>
 
-           <IconButton
+          <IconButton
             aria-label="open menu"
             mr="4"
             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            display={["inline", "inline", "none", "none",]}
-            onClick={() => setIsOpen(!isOpen)} />
+            display={["inline", "inline", "none", "none"]}
+            onClick={() => setIsOpen(!isOpen)}
+          />
 
-
-          {isOpen && <Box position="absolute" top="60px" right="100px" display={["inline", "inline", "none", "none",]} >
-            <Flex direction="column"  shadow="lg"  justify="strech" width="100px" borderRadius="md" >
-              <Link href="/blog" my="2" textAlign="center" textDecor="none" outline="none"  >
-                Blogs
-              </Link>
-              <Divider/>
-              <Link href="/projects" my="2" textAlign="center" textDecor="none" outline="none" >
-                Projects 
-              </Link>
-            </Flex>
-          </Box>}
+          {isOpen && (
+            <Box
+              position="absolute"
+              top="60px"
+              right="100px"
+              display={["inline", "inline", "none", "none"]}>
+              <Flex
+                direction="column"
+                shadow="lg"
+                justify="strech"
+                width="100px"
+                borderRadius="md">
+                <Link
+                  href="/blog"
+                  my="2"
+                  textAlign="center"
+                  textDecor="none"
+                  outline="none">
+                  Blogs
+                </Link>
+                <Divider />
+                <Link
+                  href="/projects"
+                  my="2"
+                  textAlign="center"
+                  textDecor="none"
+                  outline="none">
+                  Projects
+                </Link>
+              </Flex>
+            </Box>
+          )}
 
           <IconButton
             onClick={() => toggleColorMode()}
             aria-label="change color mode"
             icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
           />
-
         </div>
       </Flex>
     </Box>
--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,41 +1,55 @@
 import React, { ContextType, FC } from "react";
-import { IPost } from './index';
-import fs from 'fs'
-import path from 'path'
+import { IPost } from "./index";
+import fs from "fs";
+import path from "path";
 import matter from "gray-matter";
 import { Box, Text } from "@chakra-ui/react";
-import Image from "next/image"
-import { marked } from 'marked'
-const Blog: FC<{ slug: string, content: string, metadata: IPost }> = ({ slug, metadata, content }) => {
+import Image from "next/image";
+import { marked } from "marked";
+const Blog: FC<{ slug: string; content: string; metadata: IPost }> = ({
+  slug,
+  metadata,
+  content,
+}) => {
   console.log(metadata);
 
-  return !metadata ? <p>Loading...</p> : <Box mt="100px">
-    <Text fontSize="2xl" fontWeight="bold" mb="8" textAlign="center" >{metadata.title}</Text>
-    <Image width="1000px" height="400px" src={metadata.coverImage} />
-    <div>
-      <div dangerouslySetInnerHTML={{ __html: marked.parse(content) }} ></div>
-    </div>
-  </Box>
+  return !metadata ? (
+    <p>Loading...</p>
+  ) : (
+    <Box mt="100px">
+      <Text fontSize="2xl" fontWeight="bold" mb="8" textAlign="center">
+        {metadata.title}
+      </Text>
+      <Image
+        width="1000px"
+        height="400px"
+        src={metadata.coverImage}
+        alt="logo"
+      />
+      <div>
+        <div dangerouslySetInnerHTML={{ __html: marked.parse(content) }}></div>
+      </div>
+    </Box>
+  );
 };
 
-
 export async function getStaticProps(context: any) {
-  const slug = context.params.slug
-  const markdownWithMeta = fs.readFileSync(path.join('posts', slug + '.md'));
-  const { data: metadata, content } = matter(markdownWithMeta)
-  return { props: { slug, content, metadata } }
+  const slug = context.params.slug;
+  const markdownWithMeta = fs.readFileSync(path.join("posts", slug + ".md"));
+  const { data: metadata, content } = matter(markdownWithMeta);
+  return { props: { slug, content, metadata } };
 }
 
 export async function getStaticPaths() {
-  const files = fs.readdirSync(path.join('posts'));
+  const files = fs.readdirSync(path.join("posts"));
   const paths = files.map((filename) => {
-    let finalName = filename.replace('.md', '')
-   return {
+    let finalName = filename.replace(".md", "");
+    return {
       params: {
-        slug: finalName
-      }
-    }
-  })
+        slug: finalName,
+      },
+    };
+  });
   return { paths, fallback: true };
 }
 
