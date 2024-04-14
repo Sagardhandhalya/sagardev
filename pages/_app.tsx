01c97a1e58d7589137b5787b014f5409adacd8f0
Sagar <dhandhalyasagar0@gmail.com>
2024-04-15 00:20:25 +0530

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,19 +4,27 @@ import { ChakraProvider, CSSReset } from "@chakra-ui/react";
 import theme from "../styles/theme";
 import Layout from "../components/Layout";
 import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-ETNNYD0N93"></Script>
+      <Script id="google-analytics">
+        {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ETNNYD0N93');`}
+      </Script>
       <Head>
         <title>Sagar Dev</title>
         <meta name="description" content="Portfolio of Sagar dhandhalya" />
       </Head>
       <Layout>
-         <CSSReset />
+        <CSSReset />
         <Component {...pageProps} />
       </Layout>
-     
     </ChakraProvider>
   );
 }
